0ecd609b2a083098e937fe23b0cde7b18736f2cc
elliot <eal001@ucsd.edu>
2022-12-10 15:17:37 -0800

    comment initialization steps on main

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
+    // load in project cards
     
+    // get parent
+
+    // get each project's data 
+
+    // load data into project card
+
+    // append the project? 
 }
